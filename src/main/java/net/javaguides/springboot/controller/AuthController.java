7000b559b8a112c7eb062a3eaa9cbb2b751fd092
Rifqi Haikal Chairiansyah <112598058+Rifqi-HaikalCh@users.noreply.github.com>
2024-07-19 14:23:46 +0700

    alomst fix

--- src/main/java/net/javaguides/springboot/controller/AuthController.java
+++ src/main/java/net/javaguides/springboot/controller/AuthController.java
@@ -1,62 +1,54 @@
 package net.javaguides.springboot.controller;
 
-import net.javaguides.springboot.dto.UserDto;
-import net.javaguides.springboot.model.User;
-import net.javaguides.springboot.repository.JwtResponseRepository;
-import net.javaguides.springboot.security.JwtTokenUtil;
-import net.javaguides.springboot.model.JwtRequest;
-import net.javaguides.springboot.model.JwtResponse;
-import net.javaguides.springboot.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+// Your custom classes
+import net.javaguides.springboot.security.JwtTokenUtil;
+import net.javaguides.springboot.service.JwtUserDetailsService;
+import net.javaguides.springboot.model.JwtRequest;
+import net.javaguides.springboot.model.JwtResponse;
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.model.User;
 
 @RestController
-@CrossOrigin
-@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenUtil jwtTokenUtil;
     private final JwtUserDetailsService userDetailsService;
-    private final JwtResponseRepository jwtResponseRepository;
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil, JwtUserDetailsService userDetailsService, JwtResponseRepository jwtResponseRepository) {
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil, JwtUserDetailsService userDetailsService) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenUtil = jwtTokenUtil;
         this.userDetailsService = userDetailsService;
-        this.jwtResponseRepository = jwtResponseRepository;
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
-        // Authenticate user credentials
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) {
+        if (authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword())) {
+            // Load user details and generate JWT token
+            final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+            final String token = jwtTokenUtil.generateToken(userDetails);
 
-        // Load user details and generate JWT token
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-        final String token = jwtTokenUtil.generateToken(userDetails);
+            // Get roles of the user
+            String roles = userDetails.getAuthorities().toString();
 
-        if (token != null) {
-            // Create JwtResponse object
-            JwtResponse jwtResponse = new JwtResponse(token);
-
-            // Optionally, you could populate other fields if needed
-            // jwtResponse.setAuthenticated(true);
-            // jwtResponse.setRoles("user_role"); // Set roles as needed
-
-            // Save JwtResponse object
-            jwtResponseRepository.save(jwtResponse);
-
-            // Return JWT response
-            return ResponseEntity.ok(jwtResponse);
+            // Return JWT response with token and roles
+            JwtResponse response = new JwtResponse(token, true, roles);
+            return ResponseEntity.ok(response);
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            // Return response for unauthorized access
+            JwtResponse response = new JwtResponse("", false, "");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
     }
 
@@ -80,11 +72,12 @@ public class AuthController {
         return ResponseEntity.ok(savedUser);
     }
 
-    private void authenticate(String username, String password) throws Exception {
+    private boolean authenticate(String username, String password) {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            return true;
         } catch (Exception e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
+            return false;
         }
     }
 }
